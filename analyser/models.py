e2f3220fc1aac1513c54de65b89f8fda3dddd237
joaovitor3 <joaovytor0@gmail.com>
2021-10-01 23:21:47 -0300

    Add command to dump nba teams to the database

--- analyser/models.py
+++ analyser/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Team(models.Model):
+    name = models.CharField(max_length=50)
+    team_id = models.PositiveIntegerField()
+    abbreviation = models.CharField(max_length=3)
